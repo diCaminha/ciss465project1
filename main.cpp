18155f47ff78c7bf7c8af57a5be1d29644dfa9ae
diCaminha <dineresc@gmail.com>
2015-09-16 09:14:17 -0500

    cleaned the main fucntion

--- main.cpp
+++ main.cpp
@@ -79,37 +79,6 @@ bool loadMedia()
     return success;
 }
 
-/*
-int main( int argc, char* args[] )
-{
-	//The window we'll be rendering to
-	SDL_Window* myWindow = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screen = NULL;
-
-	//Initialize SDL
-	
-	//Create window
-	myWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 500, SDL_WINDOW_SHOWN );
-	
-	//Get window surface
-	screen = SDL_GetWindowSurface( myWindow );
-	//Fill the surface white
-	SDL_FillRect( screen, NULL, SDL_MapRGB( screen->format, 1.0f, 1.0f, 1.0f ) );
-	//Update the surface
-	SDL_UpdateWindowSurface( myWindow );
-	//Wait 4 seconds
-	SDL_Delay( 4000 );
-	//Destroy window
-	SDL_DestroyWindow( myWindow );
-
-	//Quit SDL
-	SDL_Quit();
-
-	return 0;
-}
-*/
 
 
 int main( int argc, char* args[] )
